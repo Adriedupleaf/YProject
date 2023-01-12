059d5542bc8cacbce827b87502eff051f1291f9d
Adriedupleaf <Adriedupleaf1@gmail.com>
2023-01-12 22:09:01 +0200

    Reformat backend

---
+++ WinFormsApp1/BackEnd/ControlsLayout.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace YProject.BackEnd
+{
+    internal class ControlsLayout
+    {
+        /*rivate void ShowSort()
+        {
+            SortType.Items.Clear();
+            IdComboBox.SelectedIndex = 0;
+            SortType.Items.Add("ASC");
+            SortType.Items.Add("DESC");
+            SortType.SelectedIndex = 0;
+            SortingPanel.Visible = true;
+        }
+        */
+        public void ClearPrevData(DataGridView Grid, System.Windows.Forms.ComboBox IdComboBox)
+        {
+            Grid.Columns.Clear();
+            IdComboBox.Items.Clear();
+            Grid.Rows.Clear();
+            Grid.Refresh();
+
+        }
+        public void ShowSort(System.Windows.Forms.ComboBox IdComboBox, Panel SortingPanel)
+        {
+            IdComboBox.SelectedIndex = 0;
+            SortingPanel.Visible = true;
+        }
+    }
+}
--- WinFormsApp1/Forms/TextForm.cs
+++ WinFormsApp1/Forms/TextForm.cs
@@ -6,80 +6,24 @@ namespace WinFormsApp1
 {
     public partial class TextForm : Form
     {
-        string? FileName;
-
+        DataReader DataReader=new();
+        ControlsLayout ControlsLayout=new();
         public TextForm()
         {
 
             InitializeComponent();
+            SortType.Items.Add("ASC");
+            SortType.Items.Add("DESC");
+            SortType.SelectedIndex = 0;
 
         }
 
 
         private void SelectButton_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new()
-            {
-                InitialDirectory = @"C:\",
-                Title = "Browse Text Files",
-                DefaultExt = "txt",
-                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
-                RestoreDirectory = true,
-                CheckFileExists = true,
-                CheckPathExists = true
-            };
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                textBox1.Text = openFileDialog1.FileName;
-                FileName = openFileDialog1.FileName;
-            }
-
+            DataReader.FileSearchOpen(textBox1);
         }
-        private void ReadData()
-        {
-            try
-            {
-                using (StreamReader Reader = new(FileName))
-                {
-                    string? element = Reader?.ReadLine();
-                    element = element?.Trim();
-                    string delimiters = " " + '\t';
-                    string[] Header = element.Split(delimiters.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                    foreach (string header in Header)
-                    {
-                        dataGridView1.Columns.Add(header, header);
-                        IdComboBox.Items.Add(header);
-                    }
-                    string[] TopBase = new string[Regex.Matches(element, @"\b\w+\b").Count];
-                    while (!Reader.EndOfStream)
-                    {
-                        element = Reader.ReadLine();
-                        _ = element.Trim();
-
-                        TopBase = element.Split(delimiters.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                        if (TopBase.Length == 0) continue;
-                        else
-                            dataGridView1.Rows.Add(TopBase);
-                    }
-                    ShowSort();
-
-                }
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-        }
-        private void ReadData(String[] Array)
-        {
-            foreach (String str in Array)
-            {
-                dataGridView1.Rows.Add(str);
-
-            }
-
-
-        }
+       
         private void ClearPrevData(bool control)
         {
             if (control)
@@ -91,24 +35,15 @@ namespace WinFormsApp1
             dataGridView1.Refresh();
 
         }
-        private void ShowSort()
-        {
-            SortType.Items.Clear();
-            IdComboBox.SelectedIndex = 0;
-            SortType.Items.Add("ASC");
-            SortType.Items.Add("DESC");
-            SortType.SelectedIndex = 0;
-            SortingPanel.Visible = true;
-        }
 
         private void OkButton_Click(object sender, EventArgs e)
         {
 
             if (File.Exists(textBox1.Text))
             {
-                ClearPrevData(true);
-                FileName = textBox1.Text;
-                ReadData();
+                ControlsLayout.ClearPrevData(dataGridView1,IdComboBox);
+                if (DataReader.ReadData(dataGridView1, IdComboBox))
+                    ControlsLayout.ShowSort(IdComboBox,SortingPanel);
                 SortButton2.Visible = true;
             }
             else MessageBox.Show("File dont exists\nPlease select .txt file");
